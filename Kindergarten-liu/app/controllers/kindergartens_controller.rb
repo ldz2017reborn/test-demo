a4c02afb0c1afd0c5a8d7eb9537c26354a040855
刘德政 <liudezheng@liudezhengdeMacBook-Pro.local>
2017-05-09 15:09:02 +0800

    implement groups#create

--- Kindergarten-liu/app/controllers/kindergartens_controller.rb
+++ Kindergarten-liu/app/controllers/kindergartens_controller.rb
@@ -6,4 +6,17 @@ class KindergartensController < ApplicationController
   def new
     @kindergarten = Kindergarten.new
   end
+
+  def create
+    @kindergarten = Kindergarten.new(kindergarten_params)
+    @kindergarten.save
+      redirect_to kindergartens_path
+  end
+
+  private
+
+  def kindergarten_params
+    params.require(:kindergarten).permit(:title, :location, :price)
+  end
+
 end
